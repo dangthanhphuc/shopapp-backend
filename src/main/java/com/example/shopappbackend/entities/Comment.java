643b6149f4fa05e46bb3671be4c4d6edf2aec94c
phucdang01235 <phucdang01235@gmail.com>
2024-01-29 10:16:40 +0700

    Add Comment entity

---
+++ src/main/java/com/example/shopappbackend/entities/Comment.java
@@ -0,0 +1,29 @@
+package com.example.shopappbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
