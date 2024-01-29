835a6b51213e2f42e73c1e7b912bc0820baa9697
phucdang01235 <phucdang01235@gmail.com>
2024-01-29 10:36:17 +0700

    Update file name

--- src/main/java/com/example/shopappbackend/repositories/OrderRepository.java
+++ src/main/java/com/example/shopappbackend/repositories/OrderRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Order extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<OrderRepository, Long> {
 }
